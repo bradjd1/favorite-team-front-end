6118ed701e128480daabe38b6ddde8ebda1aa48f
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-09-30 19:11:44 -0500

    work on css styling. Put Team page in grids and add media query

--- src/components/AllTeams/AllTeams.js
+++ src/components/AllTeams/AllTeams.js
@@ -5,35 +5,44 @@ import { Link } from 'react-router-dom';
 export default function AllTeams(props) {
     const teams = props.teams.map((team) => {
         return (
-            <li key={team.id}>
-                <Link to={`/teams/${team.id}`}>{team.name}</Link>
-                &nbsp; {team.sport} &nbsp; {team.league} &nbsp; {team.city}
-                &nbsp; 
-                <button onClick={() =>props.deleteTeam(team.id)}>Delete</button>
-            </li>
+                <div key={team.id} className='grid-container'>
+                    <div className='listName'>
+                      <Link to={`/teams/${team.id}`}>{team.name}</Link>
+                    </div>
+                    <div className='listSport'>{team.sport}</div>
+                    <div className='listLeague'>{team.league}</div>
+                    <div className='listCity'>{team.city}</div>
+                    <button onClick={() => props.deleteTeam(team.id)}>Delete</button>
+
+                </div>
         )
     });
     return (
         <div className='AllTeams'>
-        <div className='innerTeam'>
-            <h2>Favorite Teams</h2>
-            <form onSubmit={props.addTeam}>
-                <div>
-                <label for='team'> &nbsp; &nbsp; &nbsp; Team: </label>
-                <input type='text' name='name' />
-                <label for='sport'> Sport: </label>
-                <input type='text' name='sport' />
-                </div>
-                <div>
-                <label for='league'> League: </label>
-                <input type='text' name='league' />
-                <label for='city'> City: </label>
-                <input type='text' name='city' />
-               </div>
-                <input type='submit' value='Add Team' />
-            </form>
-            <ul className='teams'>{teams}</ul>
-        </div>
+            <div className='innerTeam'>
+                <h2>Favorite Teams</h2>
+                <form onSubmit={props.addTeam}>
+                    <div className='submitField'>
+                        <label for='team'>Team</label>
+                        <input type='text' name='name' />
+                    </div>
+                    <div className='submitField'>
+                        <label for='sport'>Sport</label>
+                        <input type='text' name='sport' />
+                    </div>
+                    <div className='submitField'>
+                        <label for='league'>League</label>
+                        <input type='text' name='league' />
+                    </div>
+                    <div className='submitField'>
+                        <label for='city'>City</label>
+                        <input type='text' name='city' />
+                    </div>
+                        <input type='submit' value='Add Team' />
+                  
+                </form>
+                <ul className='teams'>{teams}</ul>
+            </div>
         </div>
     );
 }
