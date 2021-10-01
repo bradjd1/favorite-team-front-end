996d114151ba58c70c390308221ead780cd74ed5
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-10-01 11:23:20 -0500

    Worked on CSS styling on favorite teams when shrinking screen

--- src/components/AllTeams/AllTeams.js
+++ src/components/AllTeams/AllTeams.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 export default function AllTeams(props) {
     const teams = props.teams.map((team) => {
         return (
+                
                 <div key={team.id} className='grid-container'>
                     <div className='listName'>
                       <Link to={`/teams/${team.id}`}>{team.name}</Link>
@@ -22,24 +23,26 @@ export default function AllTeams(props) {
             <div className='innerTeam'>
                 <h2>Favorite Teams</h2>
                 <form onSubmit={props.addTeam}>
-                    <div className='submitField'>
+                    <div className='submitField field'>
                         <label for='team'>Team</label>
                         <input type='text' name='name' />
                     </div>
-                    <div className='submitField'>
+                    <div className='submitField sport'>
                         <label for='sport'>Sport</label>
                         <input type='text' name='sport' />
                     </div>
-                    <div className='submitField'>
+                    <div className='submitField league'>
                         <label for='league'>League</label>
                         <input type='text' name='league' />
                     </div>
-                    <div className='submitField'>
+                    <div className='submitField city'>
                         <label for='city'>City</label>
                         <input type='text' name='city' />
                     </div>
-                        <input type='submit' value='Add Team' />
-                  
+                    <div className='sub'>
+                        <br/>
+                        <input className='button' type='submit' value='Add Team' />
+                    </div>
                 </form>
                 <ul className='teams'>{teams}</ul>
             </div>
